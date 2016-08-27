b680ca6808dca1a51381d6f7240b316746b93363
csandr1970 <csandr1970@rambler.ru>
2016-08-27 17:26:06 +0300

    HomeworkJS_1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function pow(base, power) {
+	var result = 1;
+
+	for (var i = 0; i < power; i++){
+		result *= base;
+	}
+
+	return result;
+}
+
+var base = + prompt('Введите число, которое необходимо возвести в степень', '00');
+var power = + prompt('Введите степень (целое положительное число)', '00');
+
+if (power < 0 || power - Math.ceil(power)!=0) {
+		alert('Введите степень - целое положительное число');
+} else {
+		alert(pow(base, power));
+		console.log( pow(base, power) );
+};
+/*if (power=0) result = 1;
+*/
